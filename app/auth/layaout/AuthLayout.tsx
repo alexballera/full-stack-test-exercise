87bfbbb719cb10fee139e6f8a5c10bbbab12d3e2
Alex Ballera <alexballera@gmail.com>
2024-10-12 13:41:36 -0300

    layout

---
+++ app/auth/layaout/AuthLayout.tsx
@@ -0,0 +1,32 @@
+import { Card, CardContent, CardHeader, Grid2 } from "@mui/material"
+import { ReactNode } from "react"
+
+type Props = {
+  children: ReactNode
+  title: string
+}
+
+const AuthLayout = ({ children, title }: Props) => {
+  return (
+    <Grid2
+      container
+      spacing={0}
+      direction={"column"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      sx={{ minHeight: '100vh', p: 4, backgroundColor: 'primary.dark' }}
+    >
+      <Grid2
+        size={{ xs: 12, sm: 8, md: 4 }}
+      >
+        <Card>
+          <CardHeader title={title} sx={{ color: 'primary.dark', p: 4, pb: 0 }} />
+          <CardContent>
+            {children}
+          </CardContent>
+        </Card>
+      </Grid2>
+    </Grid2>
+  )
+}
+export default AuthLayout
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import LoginIcon from '@mui/icons-material/Login'
 import LoadingButton from '@mui/lab/LoadingButton'
-import { Card, CardActions, CardContent, CircularProgress, FormControl, FormHelperText, Grid2, IconButton, InputAdornment, TextField, Typography } from "@mui/material"
+import { CardActions, CardContent, CircularProgress, FormControl, FormHelperText, Grid2, IconButton, InputAdornment, TextField } from "@mui/material"
 
 // ** Third Party Imports
 import { yupResolver } from '@hookform/resolvers/yup'
@@ -9,6 +9,7 @@ import { Google, Visibility, VisibilityOff } from '@mui/icons-material'
 import Link from 'next/link'
 import { Suspense, useState } from "react"
 import { Controller, useForm } from 'react-hook-form'
+import AuthLayout from '../auth/layaout/AuthLayout'
 import { defaultValues, Login, schema } from './model'
 
 function LoginPage() {
@@ -37,142 +38,125 @@ function LoginPage() {
 
   return (
     <Suspense fallback={<CircularProgress disableShrink sx={{ mt: 6 }} />}>
-
-      <Grid2
-        container
-        spacing={0}
-        direction={"column"}
-        alignItems={"center"}
-        justifyContent={"center"}
-        sx={{ minHeight: '100vh', p: 4, backgroundColor: 'primary.dark' }}
-      >
-        <Grid2
-          size={{ xs: 12, sm: 8, md: 4 }}
-          sx={{ backgroundColor: 'primary.main', borderRadius: 2 }}
-        >
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Card>
-              <CardContent>
-                <Grid2 container spacing={2}>
-
-                  <Typography variant="h5" sx={{ mb: 1, color: 'primary.dark' }}>Login</Typography>
-                  <Grid2 size={{ xs: 12 }}>
-                    <FormControl fullWidth>
-                      <Controller
-                        name='email'
-                        control={control}
-                        render={({ field: { value, onChange } }) => (
-                          <TextField
-                            value={value}
-                            label='Email'
-                            onChange={onChange}
-                            type='email'
-                            placeholder='Escriba correo electrónico'
-                            aria-describedby='correo-electronico'
-                            disabled={loading}
-                            color='secondary'
-                            error={Boolean(errors.email)}
-                            sx={{ color: 'primary.dark' }}
-                          />
-                        )}
+      <AuthLayout title='Login'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <CardContent>
+            <Grid2 container spacing={2}>
+              <Grid2 size={{ xs: 12 }}>
+                <FormControl fullWidth>
+                  <Controller
+                    name='email'
+                    control={control}
+                    render={({ field: { value, onChange } }) => (
+                      <TextField
+                        value={value}
+                        label='Email'
+                        onChange={onChange}
+                        type='email'
+                        placeholder='Escriba correo electrónico'
+                        aria-describedby='correo-electronico'
+                        disabled={loading}
+                        color='secondary'
+                        error={Boolean(errors.email)}
+                        sx={{ color: 'primary.dark' }}
                       />
-                      {errors.email && (
-                        <FormHelperText sx={{ color: 'error.main' }} id='validation-schema-moneda'>
-                          {errors.email.message}
-                        </FormHelperText>
-                      )}
-                    </FormControl>
-                  </Grid2>
-                  <Grid2 size={{ xs: 12 }}>
-                    <FormControl fullWidth>
-                      <Controller
-                        name='password'
-                        control={control}
-                        render={({ field: { value, onChange } }) => (
-                          <TextField
-                            value={value}
-                            label='Contraseña'
-                            onChange={onChange}
-                            type={showPassword ? 'text' : 'password'}
-                            placeholder='Escriba contraseña'
-                            aria-describedby='password'
-                            disabled={loading}
-                            color='secondary'
-                            sx={{ color: 'primary.dark' }}
-                            error={Boolean(errors.password)}
-                            slotProps={{
-                              input: {
-                                endAdornment: (
-                                  <InputAdornment position="end">
-                                    <IconButton
-                                      aria-label="toggle password visibility"
-                                      onClick={() => setShowPassword(!showPassword)}
-                                      onMouseDown={() => setShowPassword(!showPassword)}
-                                    >
-                                      {showPassword ? <Visibility sx={{ color: 'primary.main' }} /> : <VisibilityOff sx={{ color: 'primary.main' }} />}
-                                    </IconButton>
-                                  </InputAdornment>
-                                )
-                              }
-                            }}
-                          />
-                        )}
+                    )}
+                  />
+                  {errors.email && (
+                    <FormHelperText sx={{ color: 'error.main' }} id='validation-schema-moneda'>
+                      {errors.email.message}
+                    </FormHelperText>
+                  )}
+                </FormControl>
+              </Grid2>
+              <Grid2 size={{ xs: 12 }}>
+                <FormControl fullWidth>
+                  <Controller
+                    name='password'
+                    control={control}
+                    render={({ field: { value, onChange } }) => (
+                      <TextField
+                        value={value}
+                        label='Contraseña'
+                        onChange={onChange}
+                        type={showPassword ? 'text' : 'password'}
+                        placeholder='Escriba contraseña'
+                        aria-describedby='password'
+                        disabled={loading}
+                        color='secondary'
+                        sx={{ color: 'primary.dark' }}
+                        error={Boolean(errors.password)}
+                        slotProps={{
+                          input: {
+                            endAdornment: (
+                              <InputAdornment position="end">
+                                <IconButton
+                                  aria-label="toggle password visibility"
+                                  onClick={() => setShowPassword(!showPassword)}
+                                  onMouseDown={() => setShowPassword(!showPassword)}
+                                >
+                                  {showPassword ? <Visibility sx={{ color: 'primary.main' }} /> : <VisibilityOff sx={{ color: 'primary.main' }} />}
+                                </IconButton>
+                              </InputAdornment>
+                            )
+                          }
+                        }}
                       />
-                      {errors.password && (
-                        <FormHelperText sx={{ color: 'error.main' }} id='validation-schema-moneda'>
-                          {errors.password.message}
-                        </FormHelperText>
-                      )}
-                    </FormControl>
-                  </Grid2>
-                </Grid2>
-              </CardContent>
-              <CardActions>
-                <Grid2
-                  container
-                  direction={"row"}
-                  alignItems={"center"}
-                  justifyContent={"flex-end"}
-                  size={{ xs: 12 }}
-                  spacing={2}
+                    )}
+                  />
+                  {errors.password && (
+                    <FormHelperText sx={{ color: 'error.main' }} id='validation-schema-moneda'>
+                      {errors.password.message}
+                    </FormHelperText>
+                  )}
+                </FormControl>
+              </Grid2>
+            </Grid2>
+          </CardContent>
+          <CardActions sx={{ p: 2 }}>
+            <Grid2
+              container
+              direction={"row"}
+              alignItems={"center"}
+              justifyContent={"flex-end"}
+              size={{ xs: 12 }}
+              spacing={2}
+            >
+              <Grid2 size={{ xs: 12, sm: 6 }}>
+                <LoadingButton
+                  fullWidth
+                  size='large'
+                  type='submit'
+                  loading={loading}
+                  color={'primary'}
+                  variant={'contained'}
+                  loadingPosition={'end'}
+                  endIcon={<Google />}
                 >
-                  <Grid2 size={{ xs: 12, md: 6 }}>
-                    <LoadingButton
-                      fullWidth
-                      size='large'
-                      type='submit'
-                      loading={loading}
-                      color={'primary'}
-                      variant={'contained'}
-                      loadingPosition={'end'}
-                      endIcon={<Google />}
-                    >
-                      Google
-                    </LoadingButton>
-                  </Grid2>
-                  <Grid2 size={{ xs: 12, md: 6 }}>
-                    <LoadingButton
-                      fullWidth
-                      size='large'
-                      type='submit'
-                      loading={loading}
-                      color={'primary'}
-                      variant={'contained'}
-                      loadingPosition={'end'}
-                      endIcon={<LoginIcon />}
-                    >
-                      Login
-                    </LoadingButton>
-                  </Grid2>
-                  <Grid2 size={{ xs: 8 }} sx={{ textAlign: 'right' }}>
-                    <Link href="/register">Crear una cuenta</Link>
-                  </Grid2>
-                </Grid2>
-              </CardActions>
-            </Card>
-          </form >
-        </Grid2>
-      </Grid2>
+                  Google
+                </LoadingButton>
+              </Grid2>
+              <Grid2 size={{ xs: 12, sm: 6 }}>
+                <LoadingButton
+                  fullWidth
+                  size='large'
+                  type='submit'
+                  loading={loading}
+                  color={'primary'}
+                  variant={'contained'}
+                  loadingPosition={'end'}
+                  endIcon={<LoginIcon />}
+                >
+                  Login
+                </LoadingButton>
+              </Grid2>
+              <Grid2 size={{ xs: 12, sm: 8 }} sx={{ textAlign: 'right' }}>
+                <Link href="/register">Crear una cuenta</Link>
+              </Grid2>
+            </Grid2>
+          </CardActions>
+        </form >
+      </AuthLayout>
     </Suspense >
   )
 }
